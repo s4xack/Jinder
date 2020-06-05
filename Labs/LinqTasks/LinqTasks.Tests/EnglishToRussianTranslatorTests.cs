c2534548d672d1d5ce1ef1b27392eeb701c514d7
Sergey Mironets <Sergey.Mironec@veeamacademy.ru>
2020-06-05 22:14:50 +0000

    feat: added tests for fourth task
    git-tfs-id: [https://dev.azure.com/veeamacademy/]$/Sergey.Mironec;C646

---
+++ Labs/LinqTasks/LinqTasks.Tests/EnglishToRussianTranslatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Translator;
+
+namespace LinqTasks.Tests
+{
+    [TestFixture]
+    public class EnglishToRussianTranslatorTests
+    {
+        [Test]
+        public void Translate_should_return_expected_answers_for_example_sentence()
+        {
+            // Arrange
+            String text = "This dog eats too much vegetables after lunch";
+            Dictionary<String, String> dictionary = new Dictionary<String, String>()
+            {
+                {"this", "эта"}, 
+                {"dog", "собака"},
+                {"eats", "ест"},
+                {"too", "слишком"},
+                {"much", "много"},
+                {"vegetables", "овощей"},
+                {"after", "после"},
+                {"lunch", "обеда"}
+            };
+            String expected = "ЭТА СОБАКА ЕСТ\nСЛИШКОМ МНОГО ОВОЩЕЙ\nПОСЛЕ ОБЕДА";
+            // Act
+            String result = EnglishToRussianTranslator.Translate(text, dictionary);
+            // Assert
+            Assert.That(result == expected);
+        }
+    }
+}
