dae5a6f597be0ced98a9676a77d946bc33329882
Sergey Mironets <Sergey.Mironec@veeamacademy.ru>
2020-07-17 00:38:48 +0000

    feat: added concurent buffer
    git-tfs-id: [https://dev.azure.com/veeamacademy/]$/Sergey.Mironec;C1095

---
+++ Labs/MultithreadingTasks/ProvidersConsumers/Buffer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ProvidersConsumers
+{
+    public class Buffer<TValue>
+    {
+        private readonly Queue<TValue> _data;
+
+        private readonly Semaphore _readAccess;
+        private readonly Semaphore _writeAccess;
+
+        private readonly Mutex _dataAccess;
+
+        public Buffer(Int32 capacity)
+        {
+            _data = new Queue<TValue>(capacity);
+            
+            _dataAccess = new Mutex();
+
+            _readAccess = new Semaphore(capacity, capacity);
+            _writeAccess = new Semaphore(0, capacity);
+        }
+
+        public void Write(TValue value)
+        {
+            _writeAccess.WaitOne();
+
+            _dataAccess.WaitOne();
+            _data.Enqueue(value);
+            _dataAccess.ReleaseMutex();
+ 
+            _readAccess.Release();
+        }
+
+        public TValue Read()
+        {
+            _readAccess.WaitOne();
+
+            _dataAccess.WaitOne();
+            TValue value = _data.Dequeue();
+            _dataAccess.ReleaseMutex();
+
+            _writeAccess.Release();
+
+            return value;
+        }
+    }
+}
